e876d93005e3313fbb0698c87cae8571c6af4c97
cp6 <corbin@corbpie.com>
2021-01-30 01:32:07 +1100

    Fixed DNS API GET requests
    Fixed DNS API GET requests

--- calls.php
+++ calls.php
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                 $idle->viewMoreDomainModal($_GET['id']);//View more details modal
             }
         } elseif ($_GET['type'] == 'dns_search') {
-            $idle->getIpForDomain($_GET['hostname'], $_GET['type']);
+            $idle->getIpForDomain($_GET['hostname'], $_GET['dns_type']);
         } elseif ($_GET['type'] == 'check_up') {
             echo $idle->checkIsUp($_GET['host']);
         } elseif ($_GET['type'] == 'object_tables') {
