dc78044e47339a77c2f694b704743df13b0b3f6f
cp6 <cp6@users.noreply.github.com>
2022-03-02 23:57:45 +1100

    Added locations view
    Added locations view

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Locations;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class LocationsController extends Controller
@@ -31,6 +32,28 @@ class LocationsController extends Controller
 
         return redirect()->route('locations.index')
             ->with('success', 'Location Created Successfully.');
+    }
+
+    public function show(Locations $location)
+    {
+        $servers = DB::table('servers as s')
+            ->where('s.location_id', '=', $location->id)
+            ->get(['s.id', 's.hostname'])
+            ->toArray();
+
+        $shared = DB::table('shared_hosting as s')
+            ->where('s.location_id', '=', $location->id)
+            ->get(['s.id', 's.main_domain as main_domain_shared'])
+            ->toArray();
+
+        $reseller = DB::table('reseller_hosting as r')
+            ->where('r.location_id', '=', $location->id)
+            ->get(['r.id', 'r.main_domain as main_domain_reseller'])
+            ->toArray();
+
+        $data = array_merge($servers, $shared, $reseller);
+
+        return view('locations.show', compact(['location', 'data']));
     }
 
     public function destroy(Locations $location)
--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -32,6 +32,9 @@
                             <td class="text-nowrap">{{ $location->name }}</td>
                             <td class="text-nowrap">
                                 <form action="{{ route('locations.destroy', $location->id) }}" method="POST">
+                                    <a href="{{ route('locations.show', $location->id) }}"
+                                       class="text-body mx-1">
+                                        <i class="fas fa-eye" title="view"></i></a>
                                     <i class="fas fa-trash text-danger ms-3" @click="modalForm"
                                        id="btn-{{$location->name}}" title="{{$location->id}}"></i>
                                 </form>
