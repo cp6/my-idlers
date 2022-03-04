829df33abdd7814b6e92878fbbbb7e7640968123
cp6 <cp6@users.noreply.github.com>
2022-03-05 00:33:51 +1100

    Updated misc show blade
    Updated misc show blade

--- resources/views/misc/show.blade.php
+++ resources/views/misc/show.blade.php
@@ -5,60 +5,75 @@
     </x-slot>
     <div class="container">
         <x-card class="shadow mt-3">
-            <x-back-button>
-                <x-slot name="href">{{ route('misc.index') }}</x-slot>
-                Go back
-            </x-back-button>
-            <div class="table-responsive">
-                <table class="table table-borderless text-nowrap">
-                    <tbody>
-                    <tr>
-                        <td class="px-4 py-2 font-bold">Service</td>
-                        <td>{{ $service_extras[0]->name }}</td>
-                    </tr>
-                    <tr>
-                        <td class="px-4 py-2 font-bold">Price</td>
-                        <td>{{ $service_extras[0]->price }} {{ $service_extras[0]->currency }}</td>
-                    </tr>
-                    <tr>
-                        <td class="px-4 py-2 font-bold">Term</td>
-                        <td>{{ \App\Process::paymentTermIntToString($service_extras[0]->term) }}</td>
-                    </tr>
-                    <tr>
-                        <td class="px-4 py-2 font-bold">Owned since</td>
-                        <td>
-                            @if(!is_null($service_extras[0]->owned_since))
-                                {{ date_format(new DateTime($service_extras[0]->owned_since), 'jS F Y') }}
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="px-4 py-2 font-bold">Next due date</td>
-                        <td>
-                            @if(!is_null($service_extras[0]->next_due_date))
-                                {{ date_format(new DateTime($service_extras[0]->next_due_date), 'jS F Y') }}
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="px-4 py-2 font-bold">Created on</td>
-                        <td>
-                            @if(!is_null($service_extras[0]->created_at))
-                                {{ date_format(new DateTime($service_extras[0]->created_at), 'jS F Y') }}
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="px-4 py-2 font-bold">Last updated</td>
-                        <td>
-                            @if(!is_null($service_extras[0]->updated_at))
-                                {{ date_format(new DateTime($service_extras[0]->updated_at), 'jS F Y') }}
-                            @endif
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
+            <div class="row">
+                <div class="col-12 col-md-6 mb-2">
+                    <h2>{{ $service_extras[0]->name}}</h2>
+                </div>
+                <div class="col-12 col-md-6 text-md-end">
+                    <h6 class="text-muted pe-lg-4">{{ $service_extras[0]->service_id }}</h6>
+                    @if($service_extras[0]->active !== 1)
+                        <h6 class="text-danger pe-lg-4">not active</h6>
+                    @endif
+                </div>
             </div>
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <div class="table-responsive">
+                        <table class="table table-borderless text-nowrap">
+                            <tbody>
+                            <tr>
+                                <td class="px-2 py-2 font-bold text-muted">Service</td>
+                                <td>{{ $service_extras[0]->name }}</td>
+                            </tr>
+                            <tr>
+                                <td class="px-2 py-2 font-bold text-muted">Price</td>
+                                <td>{{ $service_extras[0]->price }} {{ $service_extras[0]->currency }}
+                                    <small>{{\App\Process::paymentTermIntToString($service_extras[0]->term)}}</small>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="px-2 py-2 font-bold text-muted">Owned since</td>
+                                <td>
+                                    @if(!is_null($service_extras[0]->owned_since))
+                                        {{ date_format(new DateTime($service_extras[0]->owned_since), 'jS F Y') }}
+                                    @endif
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="px-2 py-2 font-bold text-muted">Next due date</td>
+                                <td>{{Carbon\Carbon::parse($service_extras[0]->next_due_date)->diffForHumans()}}
+                                    ({{Carbon\Carbon::parse($service_extras[0]->next_due_date)->format('d/m/Y')}})
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="px-2 py-2 font-bold text-muted">Inserted</td>
+                                <td>
+                                    @if(!is_null($service_extras[0]->created_at))
+                                        {{ date_format(new DateTime($service_extras[0]->created_at), 'jS M y g:i a') }}
+                                    @endif
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="px-2 py-2 font-bold text-muted">Updated</td>
+                                <td>
+                                    @if(!is_null($service_extras[0]->updated_at))
+                                        {{ date_format(new DateTime($service_extras[0]->updated_at), 'jS M y g:i a') }}
+                                    @endif
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <a href="{{ route('misc.index') }}"
+               class="btn btn-success btn-sm mx-2">
+                Go back
+            </a>
+            <a href="{{ route('misc.edit', $service_extras[0]->service_id) }}"
+               class="btn btn-primary btn-sm mx-2">
+                Edit
+            </a>
         </x-card>
     </div>
 </x-app-layout>
