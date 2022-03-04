b7ce9b74026f0ad26071983fe6c14a1eec0ae147
cp6 <cp6@users.noreply.github.com>
2022-03-05 01:00:21 +1100

    Updated labels_assigned migration for unique key index
    Updated labels_assigned migration for unique key index

--- database/migrations/2022_02_08_010323_create_labels_assigned_table.php
+++ database/migrations/2022_02_08_010323_create_labels_assigned_table.php
@@ -14,8 +14,9 @@ class CreateLabelsAssignedTable extends Migration
     public function up()
     {
         Schema::create('labels_assigned', function (Blueprint $table) {
-            $table->char('label_id', 8)->unique();
-            $table->char('service_id', 8)->unique();
+            $table->char('label_id', 8);
+            $table->char('service_id', 8);
+            $table->unique(['label_id','service_id'], 'uni');
         });
     }
 
