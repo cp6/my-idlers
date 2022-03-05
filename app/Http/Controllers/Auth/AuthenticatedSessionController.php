6dfcaea04f1581a2a3831207dd8693d2bdee5211
cp6 <cp6@users.noreply.github.com>
2022-03-05 11:05:50 +1100

    If no users exist redirect to /register from /login
    If no users exist redirect to /register from /login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,6 +18,10 @@ class AuthenticatedSessionController extends Controller
      */
     public function create()
     {
+        $users = User::all();
+        if ($users->count() === 0) {//Only allow 1 user for this app
+            return redirect('/register');
+        }
         return view('auth.login');
     }
 
