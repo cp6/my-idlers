770437bc5fad3ed232d6640cafcd7033b095e592
cp6 <cp6@users.noreply.github.com>
2022-03-06 21:47:00 +1100

    Updated locations seeder array formatting
    Updated locations seeder array formatting

--- database/seeders/LocationsSeeder.php
+++ database/seeders/LocationsSeeder.php
@@ -8,264 +8,94 @@ use Illuminate\Support\Facades\DB;
 class LocationsSeeder extends Seeder
 {
     public function run()
-    {
-        $locations = array(
-            array(
-                "name" => "My basement",
-            ),
-            array(
-                "name" => "Adelaide, Australia",
-            ),
-            array(
-                "name" => "Amsterdam, Netherlands",
-            ),
-            array(
-                "name" => "Atlanta, USA",
-            ),
-            array(
-                "name" => "Auckland, New Zealand",
-            ),
-            array(
-                "name" => "Bangkok, Thailand",
-            ),
-            array(
-                "name" => "Barcelona, Spain",
-            ),
-            array(
-                "name" => "Beijing, China",
-            ),
-            array(
-                "name" => "Bengaluru, India",
-            ),
-            array(
-                "name" => "Berlin, Germany",
-            ),
-            array(
-                "name" => "Brisbane, Australia",
-            ),
-            array(
-                "name" => "Brussels, Belgium",
-            ),
-            array(
-                "name" => "Bucharest, Romania",
-            ),
-            array(
-                "name" => "Buenos Aires, Argentina",
-            ),
-            array(
-                "name" => "Charlotte, USA",
-            ),
-            array(
-                "name" => "Chennai, India",
-            ),
-            array(
-                "name" => "Chicago, USA",
-            ),
-            array(
-                "name" => "Christchurch, New Zealand",
-            ),
-            array(
-                "name" => "Dallas, USA",
-            ),
-            array(
-                "name" => "Denver, USA",
-            ),
-            array(
-                "name" => "Dhaka, Bangladesh",
-            ),
-            array(
-                "name" => "Dublin, Ireland",
-            ),
-            array(
-                "name" => "Frankfurt, Germany",
-            ),
-            array(
-                "name" => "Hamburg, Germany",
-            ),
-            array(
-                "name" => "Helsinki, Finland",
-            ),
-            array(
-                "name" => "Hong Kong",
-            ),
-            array(
-                "name" => "Houston, USA",
-            ),
-            array(
-                "name" => "Istanbul, Turkey",
-            ),
-            array(
-                "name" => "Jakarta, Indonesia",
-            ),
-            array(
-                "name" => "Johannesburg, South Africa",
-            ),
-            array(
-                "name" => "Kansas City, USA",
-            ),
-            array(
-                "name" => "Kuala Lumpur, Malaysia",
-            ),
-            array(
-                "name" => "Kiev, Ukraine",
-            ),
-            array(
-                "name" => "Las Vegas, USA",
-            ),
-            array(
-                "name" => "London, United kingdom",
-            ),
-            array(
-                "name" => "Los Angeles, USA",
-            ),
-            array(
-                "name" => "Luxembourg",
-            ),
-            array(
-                "name" => "Lyon, France",
-            ),
-            array(
-                "name" => "Madrid, Spain",
-            ),
-            array(
-                "name" => "Manchester, United Kingdom",
-            ),
-            array(
-                "name" => "Melbourne, Australia",
-            ),
-            array(
-                "name" => "Mexico City, Mexico",
-            ),
-            array(
-                "name" => "Miami, USA",
-            ),
-            array(
-                "name" => "Milan, Italy",
-            ),
-            array(
-                "name" => "Montreal, Canada",
-            ),
-            array(
-                "name" => "Moscow, Russia",
-            ),
-            array(
-                "name" => "Mumbai, India",
-            ),
-            array(
-                "name" => "Munich, Germany",
-            ),
-            array(
-                "name" => "New Delhi, India",
-            ),
-            array(
-                "name" => "New Jersey, USA",
-            ),
-            array(
-                "name" => "New York, USA",
-            ),
-            array(
-                "name" => "Newcastle, United Kingdom",
-            ),
-            array(
-                "name" => "Nuremberg, Germany",
-            ),
-            array(
-                "name" => "Ogden, USA",
-            ),
-            array(
-                "name" => "Oslo, Norway",
-            ),
-            array(
-                "name" => "Paris, France",
-            ),
-            array(
-                "name" => "Perth, Australia",
-            ),
-            array(
-                "name" => "Phoenix, USA",
-            ),
-            array(
-                "name" => "Philadelphia, USA",
-            ),
-            array(
-                "name" => "Portland, USA",
-            ),
-            array(
-                "name" => "Riga, Latvia",
-            ),
-            array(
-                "name" => "Rome, Italy",
-            ),
-            array(
-                "name" => "Rotterdam, Netherlands",
-            ),
-            array(
-                "name" => "Salt Lake City, USA",
-            ),
-            array(
-                "name" => "San Diego, USA",
-            ),
-            array(
-                "name" => "San Francisco, USA",
-            ),
-            array(
-                "name" => "San Jose, USA",
-            ),
-            array(
-                "name" => "Seattle, USA",
-            ),
-            array(
-                "name" => "Seoul, South Korea",
-            ),
-            array(
-                "name" => "Shanghai, China",
-            ),
-            array(
-                "name" => "Silicon Valley, USA",
-            ),
-            array(
-                "name" => "Singapore",
-            ),
-            array(
-                "name" => "Sofia, Bulgaria",
-            ),
-            array(
-                "name" => "St Petersburg, Russia",
-            ),
-            array(
-                "name" => "Stockholm, Sweden",
-            ),
-            array(
-                "name" => "Sydney, Australia",
-            ),
-            array(
-                "name" => "Tampa, USA",
-            ),
-            array(
-                "name" => "Tokyo, Japan",
-            ),
-            array(
-                "name" => "Toronto, Canada",
-            ),
-            array(
-                "name" => "Vancouver, Canada",
-            ),
-            array(
-                "name" => "Warsaw, Poland",
-            ),
-            array(
-                "name" => "Washington, USA",
-            ),
-            array(
-                "name" => "Wellington, New Zealand",
-            ),
-            array(
-                "name" => "Zurich, Switzerland",
-            ),
-            array(
-                "name" => "Quebec, Canada",
-            ),
-        );
+    {//Note add any new locations at the bottom of the array
+        $locations = [
+            ['name' => 'My basement'],
+            ['name' => 'Adelaide, Australia'],
+            ['name' => 'Amsterdam, Netherlands'],
+            ['name' => 'Atlanta, USA'],
+            ['name' => 'Auckland, New Zealand'],
+            ['name' => 'Bangkok, Thailand'],
+            ['name' => 'Barcelona, Spain'],
+            ['name' => 'Beijing, China'],
+            ['name' => 'Bengaluru, India'],
+            ['name' => 'Berlin, Germany'],
+            ['name' => 'Brisbane, Australia'],
+            ['name' => 'Brussels, Belgium'],
+            ['name' => 'Bucharest, Romania'],
+            ['name' => 'Buenos Aires, Argentina'],
+            ['name' => 'Charlotte, USA'],
+            ['name' => 'Chennai, India'],
+            ['name' => 'Chicago, USA'],
+            ['name' => 'Christchurch, New Zealand'],
+            ['name' => 'Dallas, USA'],
+            ['name' => 'Denver, USA'],
+            ['name' => 'Dhaka, Bangladesh'],
+            ['name' => 'Dublin, Ireland'],
+            ['name' => 'Frankfurt, Germany'],
+            ['name' => 'Hamburg, Germany'],
+            ['name' => 'Helsinki, Finland'],
+            ['name' => 'Hong Kong'],
+            ['name' => 'Houston, USA'],
+            ['name' => 'Istanbul, Turkey'],
+            ['name' => 'Jakarta, Indonesia'],
+            ['name' => 'Johannesburg, South Africa'],
+            ['name' => 'Kansas City, USA'],
+            ['name' => 'Kuala Lumpur, Malaysia'],
+            ['name' => 'Kiev, Ukraine'],
+            ['name' => 'Las Vegas, USA'],
+            ['name' => 'London, United kingdom'],
+            ['name' => 'Los Angeles, USA'],
+            ['name' => 'Luxembourg'],
+            ['name' => 'Lyon, France'],
+            ['name' => 'Madrid, Spain'],
+            ['name' => 'Manchester, United Kingdom'],
+            ['name' => 'Melbourne, Australia'],
+            ['name' => 'Mexico City, Mexico'],
+            ['name' => 'Miami, USA'],
+            ['name' => 'Milan, Italy'],
+            ['name' => 'Montreal, Canada'],
+            ['name' => 'Moscow, Russia'],
+            ['name' => 'Mumbai, India'],
+            ['name' => 'Munich, Germany'],
+            ['name' => 'New Delhi, India'],
+            ['name' => 'New Jersey, USA'],
+            ['name' => 'New York, USA'],
+            ['name' => 'Newcastle, United Kingdom'],
+            ['name' => 'Nuremberg, Germany'],
+            ['name' => 'Ogden, USA'],
+            ['name' => 'Oslo, Norway'],
+            ['name' => 'Paris, France'],
+            ['name' => 'Perth, Australia'],
+            ['name' => 'Phoenix, USA'],
+            ['name' => 'Philadelphia, USA'],
+            ['name' => 'Portland, USA'],
+            ['name' => 'Riga, Latvia'],
+            ['name' => 'Rome, Italy'],
+            ['name' => 'Rotterdam, Netherlands'],
+            ['name' => 'Salt Lake City, USA'],
+            ['name' => 'San Diego, USA'],
+            ['name' => 'San Francisco, USA'],
+            ['name' => 'San Jose, USA'],
+            ['name' => 'Seattle, USA'],
+            ['name' => 'Seoul, South Korea'],
+            ['name' => 'Shanghai, China'],
+            ['name' => 'Silicon Valley, USA'],
+            ['name' => 'Singapore'],
+            ['name' => 'Sofia, Bulgaria'],
+            ['name' => 'St Petersburg, Russia'],
+            ['name' => 'Stockholm, Sweden'],
+            ['name' => 'Sydney, Australia'],
+            ['name' => 'Tampa, USA'],
+            ['name' => 'Tokyo, Japan'],
+            ['name' => 'Toronto, Canada'],
+            ['name' => 'Vancouver, Canada'],
+            ['name' => 'Warsaw, Poland'],
+            ['name' => 'Washington, USA'],
+            ['name' => 'Wellington, New Zealand'],
+            ['name' => 'Zurich, Switzerland'],
+            ['name' => 'Quebec, Canada']
+        ];
 
         DB::table('locations')->insert($locations);
 
