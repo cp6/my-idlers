fb22a50ff69ea338471a57a4af96f337bae19e89
cp6 <cp6@users.noreply.github.com>
2022-05-16 15:39:10 +1000

    Updated foreign keys
    Updated foreign keys: Removed location, provider and OS id keys

--- database/migrations/2022_05_09_114227_add_foreign_keys.php
+++ database/migrations/2022_05_09_114227_add_foreign_keys.php
@@ -9,26 +9,26 @@ class AddForeignKeys extends Migration
     public function up()
     {
         Schema::table('servers', function (Blueprint $table) {
-            $table->foreign('location_id','servers_fk_location')->references('id')->on('locations');
-            $table->foreign('provider_id','servers_fk_provider')->references('id')->on('providers');
-            $table->foreign('os_id','servers_fk_os')->references('id')->on('os');
+            //$table->foreign('location_id','servers_fk_location')->references('id')->on('locations');
+            //$table->foreign('provider_id','servers_fk_provider')->references('id')->on('providers');
+            //$table->foreign('os_id','servers_fk_os')->references('id')->on('os');
             $table->foreign('id', 'servers_fk_pricing')->references('service_id')->on('pricings')->onDelete('cascade');
         });
 
         Schema::table('shared_hosting', function (Blueprint $table) {
-            $table->foreign('location_id','shared_fk_location')->references('id')->on('locations');
-            $table->foreign('provider_id','shared_fk_provider')->references('id')->on('providers');
+            //$table->foreign('location_id','shared_fk_location')->references('id')->on('locations');
+            //$table->foreign('provider_id','shared_fk_provider')->references('id')->on('providers');
             $table->foreign('id', 'shared_fk_pricing')->references('service_id')->on('pricings')->onDelete('cascade');
         });
 
         Schema::table('reseller_hosting', function (Blueprint $table) {
-            $table->foreign('location_id','reseller_fk_location')->references('id')->on('locations');
-            $table->foreign('provider_id','reseller_fk_provider')->references('id')->on('providers');
+            //$table->foreign('location_id','reseller_fk_location')->references('id')->on('locations');
+            //$table->foreign('provider_id','reseller_fk_provider')->references('id')->on('providers');
             $table->foreign('id', 'reseller_fk_pricing')->references('service_id')->on('pricings')->onDelete('cascade');
         });
 
         Schema::table('domains', function (Blueprint $table) {
-            $table->foreign('provider_id','domains_fk_provider')->references('id')->on('providers');
+            //$table->foreign('provider_id','domains_fk_provider')->references('id')->on('providers');
             $table->foreign('id', 'domains_fk_pricing')->references('service_id')->on('pricings')->onDelete('cascade');
         });
 
