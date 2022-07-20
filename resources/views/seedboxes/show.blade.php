c9108efd63575f6712542cc1f7c96836e4bfa3d7
cp6 <cp6@users.noreply.github.com>
2022-07-20 14:35:37 +1000

    Updated SeedBoxes class to use relationships + cache
    Updated SeedBoxes class to use relationships + cache

--- resources/views/seedboxes/show.blade.php
+++ resources/views/seedboxes/show.blade.php
@@ -1,4 +1,4 @@
-@section('title') {{$seedbox->title}} {{'Seed box'}} @endsection
+@section('title') {{$seedbox_data->title}} {{'Seed box'}} @endsection
 <x-app-layout>
     <x-slot name="header">
         {{ __('Seed box details') }}
@@ -7,18 +7,14 @@
         <x-card class="shadow mt-3">
             <div class="row">
                 <div class="col-12 col-md-6 mb-2">
-                    <h2>{{ $seedbox->title }}</h2>
-                    <code>@foreach($labels as $label)
-                            @if($loop->last)
-                                {{$label->label}}
-                            @else
-                                {{$label->label}},
-                            @endif
-                        @endforeach</code>
+                    <h2>{{ $seedbox_data->title }}</h2>
+                    @foreach($seedbox_data->labels as $label)
+                        <span class="badge bg-primary mx-1">{{$label->label->label}}</span>
+                    @endforeach
                 </div>
                 <div class="col-12 col-md-6 text-md-end">
-                    <h6 class="text-muted pe-lg-4">{{ $seedbox->id }}</h6>
-                    @if($seedbox->active !== 1)
+                    <h6 class="text-muted pe-lg-4">{{ $seedbox_data->id }}</h6>
+                    @if($seedbox_data->active !== 1)
                         <h6 class="text-danger pe-lg-4">not active</h6>
                     @endif
                 </div>
@@ -30,57 +26,57 @@
                             <tbody>
                             <tr>
                                 <td class="px-2 py-2 font-bold text-muted">Type</td>
-                                <td>{{ $seedbox->seed_box_type }}</td>
+                                <td>{{ $seedbox_data->seed_box_type }}</td>
                             </tr>
                             <tr>
                                 <td class="px-2 py-2 font-bold text-muted">Hostname</td>
-                                <td><code>{{ $seedbox_extras[0]->hostname }}</code></td>
+                                <td><code>{{ $seedbox_data->hostname }}</code></td>
                             </tr>
                             <tr>
                                 <td class="px-2 py-2 font-bold text-muted">Location</td>
-                                <td>{{ $seedbox_extras[0]->location }}</td>
+                                <td>{{ $seedbox_data->location->name }}</td>
                             </tr>
                             <tr>
                                 <td class="px-2 py-2 font-bold text-muted">Provider</td>
-                                <td>{{ $seedbox_extras[0]->provider_name }}</td>
+                                <td>{{ $seedbox_data->provider->name }}</td>
                             </tr>
                             <tr>
                                 <td class="px-2 py-2 font-bold text-muted">Price</td>
-                                <td>{{ $seedbox_extras[0]->price }} {{ $seedbox_extras[0]->currency }}
-                                    <small>{{\App\Process::paymentTermIntToString($seedbox_extras[0]->term)}}</small>
+                                <td>{{ $seedbox_data->price->price }} {{ $seedbox_data->price->currency }}
+                                    <small>{{\App\Process::paymentTermIntToString($seedbox_data->price->term)}}</small>
                                 </td>
                             </tr>
                             <tr>
                                 <td class="px-2 py-2 font-bold text-muted">Was promo</td>
-                                <td>{{ ($seedbox_extras[0]->was_promo === 1) ? 'Yes' : 'No' }}</td>
+                                <td>{{ ($seedbox_data->was_promo === 1) ? 'Yes' : 'No' }}</td>
                             </tr>
                             <tr>
                                 <td class="px-2 py-2 font-bold text-muted">Owned since</td>
                                 <td>
-                                    @if(!is_null($seedbox->owned_since))
-                                        {{ date_format(new DateTime($seedbox->owned_since), 'jS F Y') }}
+                                    @if(!is_null($seedbox_data->owned_since))
+                                        {{ date_format(new DateTime($seedbox_data->owned_since), 'jS F Y') }}
                                     @endif
                                 </td>
                             </tr>
                             <tr>
                                 <td class="px-2 py-2 font-bold text-muted">Next due date</td>
-                                <td>{{Carbon\Carbon::parse($seedbox_extras[0]->next_due_date)->diffForHumans()}}
-                                    ({{Carbon\Carbon::parse($seedbox_extras[0]->next_due_date)->format('d/m/Y')}})
+                                <td>{{Carbon\Carbon::parse($seedbox_data->price->next_due_date)->diffForHumans()}}
+                                    ({{Carbon\Carbon::parse($seedbox_data->price->next_due_date)->format('d/m/Y')}})
                                 </td>
                             </tr>
                             <tr>
                                 <td class="px-2 py-2 font-bold text-muted">Inserted</td>
                                 <td>
-                                    @if(!is_null($seedbox->created_at))
-                                        {{ date_format(new DateTime($seedbox->created_at), 'jS M y g:i a') }}
+                                    @if(!is_null($seedbox_data->created_at))
+                                        {{ date_format(new DateTime($seedbox_data->created_at), 'jS M y g:i a') }}
                                     @endif
                                 </td>
                             </tr>
                             <tr>
                                 <td class="px-2 py-2 font-bold text-muted">Updated</td>
                                 <td>
-                                    @if(!is_null($seedbox->updated_at))
-                                        {{ date_format(new DateTime($seedbox->updated_at), 'jS M y g:i a') }}
+                                    @if(!is_null($seedbox_data->updated_at))
+                                        {{ date_format(new DateTime($seedbox_data->updated_at), 'jS M y g:i a') }}
                                     @endif
                                 </td>
                             </tr>
@@ -94,30 +90,30 @@
                         <tr>
                             <td class="px-2 py-2 font-bold text-muted">Disk</td>
                             <td>
-                                @if($seedbox->disk_as_gb >= 1000)
-                                    {{ number_format($seedbox->disk_as_gb / 1000,1) }} <small>TB</small>
+                                @if($seedbox_data->disk_as_gb >= 1000)
+                                    {{ number_format($seedbox_data->disk_as_gb / 1000,1) }} <small>TB</small>
                                 @else
-                                    {{ $seedbox->disk_as_gb }} <small>GB</small>
+                                    {{ $seedbox_data->disk_as_gb }} <small>GB</small>
                                 @endif
                             </td>
                         </tr>
                         <tr>
                             <td class="px-2 py-2 font-bold text-muted">Bandwidth</td>
                             <td>
-                                @if($seedbox->bandwidth >= 1000)
-                                    {{ number_format($seedbox->bandwidth / 1000,1) }} <small>TB</small>
+                                @if($seedbox_data->bandwidth >= 1000)
+                                    {{ number_format($seedbox_data->bandwidth / 1000,1) }} <small>TB</small>
                                 @else
-                                    {{ $seedbox->bandwidth }} <small>GB</small>
+                                    {{ $seedbox_data->bandwidth }} <small>GB</small>
                                 @endif
                             </td>
                         </tr>
                         <tr>
                             <td class="px-2 py-2 font-bold text-muted">Port speed</td>
                             <td>
-                                @if($seedbox->port_speed >= 1000)
-                                    {{ number_format($seedbox->port_speed / 1000,1) }} <small>Gbps</small>
+                                @if($seedbox_data->port_speed >= 1000)
+                                    {{ number_format($seedbox_data->port_speed / 1000,1) }} <small>Gbps</small>
                                 @else
-                                    {{ $seedbox->port_speed }} <small>Mbps</small>
+                                    {{ $seedbox_data->port_speed }} <small>Mbps</small>
                                 @endif
                             </td>
                         </tr>
@@ -126,14 +122,12 @@
 
                 </div>
             </div>
-            <a href="{{ route('seedboxes.index') }}"
-               class="btn btn-success btn-sm mx-2">
-                Go back
-            </a>
-            <a href="{{ route('seedboxes.edit', $seedbox->id) }}"
-               class="btn btn-primary btn-sm mx-2">
-                Edit
-            </a>
+            <x-back-btn>
+                <x-slot name="route">{{ route('seedboxes.index') }}</x-slot>
+            </x-back-btn>
+            <x-edit-btn>
+                <x-slot name="route">{{ route('seedboxes.edit', $seedbox_data->id) }}</x-slot>
+            </x-edit-btn>
         </x-card>
         @if(Session::has('timer_version_footer') && Session::get('timer_version_footer') === 1)
             <p class="text-muted mt-4 text-end"><small>
