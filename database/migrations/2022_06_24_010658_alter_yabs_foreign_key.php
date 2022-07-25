59b3138dfb0e6dc9f1ccfd8fb20d968e8b7d8d05
cp6 <cp6@users.noreply.github.com>
2022-07-25 10:33:54 +1000

    Possible YABs foreign keys migrations error fix
    Possible YABs foreign keys migrations error fix

--- database/migrations/2022_06_24_010658_alter_yabs_foreign_key.php
+++ database/migrations/2022_06_24_010658_alter_yabs_foreign_key.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -9,13 +10,17 @@ return new class extends Migration
     public function up()
     {
         Schema::table('disk_speed', function (Blueprint $table) {
-            $table->dropForeign('ds_fk_yabs');
+            if (env('DB_CONNECTION') !== 'sqlite') {
+                $table->dropForeign('ds_fk_yabs');
+            }
 
             $table->foreign('id', 'ds_fk_yabs')->references('id')->on('yabs')->onDelete('cascade');
         });
 
         Schema::table('network_speed', function (Blueprint $table) {
-            $table->dropForeign('ns_fk_yabs');
+            if (env('DB_CONNECTION') !== 'sqlite') {
+                $table->dropForeign('ns_fk_yabs');
+            }
 
             $table->foreign('id', 'ns_fk_yabs')->references('id')->on('yabs')->onDelete('cascade');
         });
