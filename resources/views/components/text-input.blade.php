5fbadf8063a1cee6fa0d64af8cd8d5556b268f32
cp6 <cp6@users.noreply.github.com>
2022-10-31 14:41:26 +1100

    Updated text input component
    Updated text input component to include placeholder and error message output if exists

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,5 +1,6 @@
 <div class="input-group">
     <div class="input-group-prepend"><span class="input-group-text">{{$title}}</span></div>
     <input type="text" name="{{$name}}" class="form-control" minlength="{{$min ?? 0}}" maxlength="{{$max ?? 99999}}"
-           value="{{$value ?? ''}}" {{(isset($required))? 'required': ''}}>
+           value="{{$value ?? ''}}" @if(isset($placeholder)) placeholder="{{$placeholder}}" @endif {{(isset($required))? 'required': ''}}>
+    @error((string)$name)<span class="text-red-500">{{ $message }}</span>@enderror
 </div>
