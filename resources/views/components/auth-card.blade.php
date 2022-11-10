0eaf09fa925b9a50edb09b815828ef7a0a193827
cp6 <cp6@users.noreply.github.com>
2022-11-10 14:53:08 +1100

    Removed logo option from auth card
    Removed logo option from auth card

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,11 +1,6 @@
 <main class="form-signin">
 <div class="card">
     <div class="card-body shadow">
-        @if(isset($logo))
-            <div>
-                {{ $logo }}
-            </div>
-        @endif
         {{ $slot }}
     </div>
 </div>
