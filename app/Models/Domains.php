966ff383202dffe744639b8ac90636017a6f82d7
cp6 <cp6@users.noreply.github.com>
2023-08-19 23:59:51 +1000

    Unused cleanup

--- app/Models/Domains.php
+++ app/Models/Domains.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class Domains extends Model
