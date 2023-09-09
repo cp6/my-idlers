3f4c46edbc95953bcf3049ec972269aeff475a07
Gamerboy59 <gamerboy59se@googlemail.com>
2023-09-09 16:13:13 +0200

    Fix for accessing an object instead of an array

--- resources/views/components/locations-select.blade.php
+++ resources/views/components/locations-select.blade.php
@@ -3,8 +3,8 @@
     <select class="form-control" name="{{$name ?? 'location_id'}}">
         @foreach ($locations as $location)
             <option
-                value="{{ $location->id }}" {{(isset($current) && (string)$current === (string)$location->id)? 'selected' : ''}}>
-                {{ $location->name }}
+                value="{{ $location['id'] }}" {{(isset($current) && (string)$current === (string)$location['id'])? 'selected' : ''}}>
+                {{ $location['name'] }}
             </option>
         @endforeach
     </select>
--- resources/views/components/providers-select.blade.php
+++ resources/views/components/providers-select.blade.php
@@ -2,8 +2,8 @@
     <div class="input-group-prepend"><span class="input-group-text">{{ $title ??'Provider'}}</span></div>
     <select class="form-control" name="{{$name ?? 'provider_id'}}">
         @foreach ($providers as $provider)
-            <option value="{{ $provider->id }}" {{(isset($current) && (string)$current === (string)$provider->id)? 'selected' : ''}}>
-                {{ $provider->name }}
+            <option value="{{ $provider['id'] }}" {{(isset($current) && (string)$current === (string)$provider['id'])? 'selected' : ''}}>
+                {{ $provider['name'] }}
             </option>
         @endforeach
     </select>
