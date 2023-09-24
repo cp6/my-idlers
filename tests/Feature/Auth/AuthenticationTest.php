f4bd6e6d66d113d5d1b0002d4cee5d248a5a4714
cp6 <cp6@users.noreply.github.com>
2023-09-24 21:35:34 +1000

    Updated tests
    Updated tests
    1 was failing due to redirect for when no users exist

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -13,6 +13,8 @@ class AuthenticationTest extends TestCase
 
     public function test_login_screen_can_be_rendered()
     {
+        $user = User::factory()->create();
+
         $response = $this->get('/login');
 
         $response->assertStatus(200);
--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -11,13 +11,6 @@ class AuthenticationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_login_screen_can_be_rendered()
-    {
-        $response = $this->get('/login');
-
-        $response->assertStatus(200);
-    }
-
     public function test_users_can_authenticate_using_the_login_screen()
     {
         $user = User::factory()->create();
