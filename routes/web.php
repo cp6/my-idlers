9d2cf82c317922c31a861ba17cd8d583943b1904
cp6 <cp6@users.noreply.github.com>
2024-05-14 22:28:05 +1000

    Removed comments from web.php

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,6 @@ use App\Http\Controllers\SharedController;
 use App\Http\Controllers\YabsController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('/');
 
