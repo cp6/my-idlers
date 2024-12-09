37032e03443a95a7e0324e1fc8636a5c3d977e9a
cp6 <cp6@users.noreply.github.com>
2024-12-09 16:52:44 +1100

    Added icon to back-button.blade.php and submit-button.blade.php

--- resources/views/components/back-button.blade.php
+++ resources/views/components/back-button.blade.php
@@ -1,1 +1,1 @@
-<a href="{{$href}}" class="btn btn-primary py-1 px-4 mb-4">{{$slot}}</a>
+<a href="{{$href}}" class="btn btn-primary py-1 px-4 mb-4"><i class="fas fa-arrow-left me-2"></i>{{$slot}}</a>
