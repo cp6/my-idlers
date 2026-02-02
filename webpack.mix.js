6e82dd69431c37122dad2097cf5bc7b4eafc0458
corbpie <me@corbpie.com>
2026-02-02 19:59:36 +1100

    4.0 - Suppress webpack and Sass deprecation warnings from third-party packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,24 +11,30 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css');
-mix.sass('resources/sass/app.scss', 'public/css').options({
-    processCssUrls: false
-}).copy(
-    'node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.ttf',
-    'public/webfonts/fa-regular-400.ttf'
-).copy(
-    'node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff2',
-    'public/webfonts/fa-regular-400.woff2'
-).copy(
-    'node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.ttf',
-    'public/webfonts/fa-solid-900.ttf'
-).copy(
-    'node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2',
-    'public/webfonts/fa-solid-900.woff2'
-).copy(
-    'node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.ttf',
-    'public/webfonts/fa-brands-400.ttf'
-);
-mix.postCss('resources/css/light.css', 'public/css');
-mix.postCss('resources/css/dark.css', 'public/css');
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css')
+    .postCss('resources/css/light.css', 'public/css')
+    .postCss('resources/css/dark.css', 'public/css', [
+        require('autoprefixer')({ overrideBrowserslist: ['last 2 versions'], grid: true })
+    ])
+    .sass('resources/sass/app.scss', 'public/css', {
+        sassOptions: {
+            quietDeps: true,
+            silenceDeprecations: ['import', 'global-builtin', 'legacy-js-api']
+        }
+    })
+    .options({
+        processCssUrls: false
+    })
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.ttf', 'public/webfonts/fa-regular-400.ttf')
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff2', 'public/webfonts/fa-regular-400.woff2')
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.ttf', 'public/webfonts/fa-solid-900.ttf')
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2', 'public/webfonts/fa-solid-900.woff2')
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-400.ttf', 'public/webfonts/fa-brands-400.ttf');
+
+mix.webpackConfig({
+    stats: {
+        children: false,
+        warnings: false
+    }
+});
