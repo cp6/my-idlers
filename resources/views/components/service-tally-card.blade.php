f251ee3af093e894a033bad4fd69842576c85b6a
corbpie <me@corbpie.com>
2026-02-02 20:32:44 +1100

    Remove unused legacy auth pages and components - Delete old Laravel UI auth pages (passwords/email, passwords/reset, passwords/confirm, verify) - Delete unused info-card and service-tally-card components - App uses newer Breeze-style auth pages

--- resources/views/components/service-tally-card.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="card">
-    <div class="card-body text-center shadow">
-        <div class="row">
-            <h3>{{ $tally }}</h3>
-            <h5><a href="{{$route}}" class="text-decoration-none">{{$service}}</a></h5>
-        </div>
-    </div>
-</div>
